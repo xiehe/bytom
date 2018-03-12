ffe51825d998075a5a4ff3d33a4d880438fa5be8
HAOYUatHZ <haoyu@protonmail.com>
2018-03-12 10:06:41 +0800

    parallelly computing matrix

--- mining/tensority/tensority.go
+++ mining/tensority/tensority.go
@@ -1,11 +1,14 @@
 package tensority
 
-import "github.com/bytom/protocol/bc"
+import (
+	"github.com/bytom/protocol/bc"
+)
 
 // Leave here for implement cache
 type AIHash struct{}
 
 func Hash(hash, seed *bc.Hash) *bc.Hash {
+
 	cache := calcSeedCache(seed.Bytes())
 	data := mulMatrix(hash.Bytes(), cache)
 
